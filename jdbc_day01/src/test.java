88577788a0f6275229ce7fccf87af3a91dbebced
YiZhiDaNengMao <1319023727@qq.com>
2022-08-25 15:59:06 +0800

    2022年8月25日15:58:42

---
+++ jdbc_day01/src/test.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author Jiang
+ * @Date 2022-08-25 15:34
+ */
+public class test {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/db_bai",
+                "root",
+                "jiang"
+        );
+
+        Statement sta = conn.createStatement();
+
+        String sql = "create table dao(id int primary key,name varchar(10)";
+
+        boolean execute = sta.execute(sql);
+        System.out.println(execute);
+
+        sta.close();
+        conn.close();
+
+
+    }
+
+}
